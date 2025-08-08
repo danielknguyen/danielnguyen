1bdf760fd334660e450ec8e17b9553633e59ea2d
Daniel Nguyen <danielknguyen@danielknguyen-MacBook-Pro.local>
2025-08-08 11:10:15 -0700

    setup slides.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,56 @@
+import { useEffect, useRef } from "react";
+import { Intro } from "pages/intro";
+import { Background } from "pages/background";
+import { Pivot } from "pages/pivot";
+import { CaseStudy } from "pages/case-study";
+import { Skills } from "pages/skills";
+import { Closing } from "pages/closing";
+import Reveal from "reveal.js";
+import "reveal.js/dist/reveal.css";
+import "reveal.js/dist/theme/black.css";
+import "./app.css";
+
 export const App = () => {
-  return <></>;
+  const deckDivRef = useRef<HTMLDivElement>(null); // reference to deck container div
+  const deckRef = useRef<Reveal.Api | null>(null); // reference to deck reveal instance
+
+  useEffect(() => {
+    // Prevents double initialization in strict mode
+    if (deckRef.current) return;
+
+    deckRef.current = new Reveal(deckDivRef.current!, {
+      transition: "slide",
+      // other config options
+    });
+
+    deckRef.current.initialize().then(() => {
+      // good place for event handlers and plugin setups
+    });
+
+    return () => {
+      try {
+        if (deckRef.current) {
+          deckRef.current.destroy();
+          deckRef.current = null;
+        }
+      } catch (e) {
+        console.warn("Reveal.js destroy call failed.");
+      }
+    };
+  }, []);
+
+  return (
+    // Your presentation is sized based on the width and height of
+    // our parent element. Make sure the parent is not 0-height.
+    <div className="reveal" ref={deckDivRef}>
+      <div className="slides">
+        <Intro />
+        <Background />
+        <Pivot />
+        <CaseStudy />
+        <Skills />
+        <Closing />
+      </div>
+    </div>
+  );
 };
